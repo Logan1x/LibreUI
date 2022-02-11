655c51aa9b00323e6dc9eacd930fe495e2c502e6
logan1x <sharmakhushal78@gmail.com>
2022-02-11 13:14:29 +0530

    Added js file for favicon

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const navbar = document.querySelector(".nav");
 const sidebar = document.querySelector(".sidebar");
+const head = document.querySelector("head");
+
+// Adding navbar to the page
 
 navbar.innerHTML =
   '<p class="nav-title"><a href="/"> ⚡ LibreUI </a><span class="text-sm">v1.0</span></p><div class="nav-pills"><a href="/installation/installation.html">Docs</a><a href="https://github.com/Logan1x/LibreUI"><i class="fab fa-github"></i></a><a href="https://twitter.com/herkuch"><i class="fab fa-twitter"></i></a></div>';
 
+// Adding sidebar to the page
+
 sidebar.innerHTML =
   '<div class="sidebar-lists"><p class="sidebar-title">Quick Start</p><a href="../installation/installation.html">Installation</a><a href="../colors/colors.html">Colors</a><a href="../typography/typography.html">Typography</a><a href="../customize/customize.html">Customize</a><a href="../contribute/contribute.html">Contribute</a><hr /><p class="sidebar-title">Components</p><a href="../alert/alert.html">Alert</a><a href="../avatar/avatar.html">Avatar</a><a href="../badge/badge.html">Bagde</a><a href="../button/button.html">Buttons</a><a href="../cards/cards.html">Cards</a><a href="../chips/chips.html">Chips</a><a href="../image/image.html">Images</a><a href="../input/input.html">Input</a><a href="../toast/toast.html">Toast</a><a href="../grid/grid.html">Grid</a><a href="../modal/modal.html">Modal</a></div>';
+
+// Adding favicon meta tags
+
+const favicon = document.createElement("link");
+favicon.rel = "shortcut icon";
+favicon.href = "../../assets/favicon.ico";
+head.appendChild(favicon);
